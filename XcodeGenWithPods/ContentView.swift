076c7aea7aaf8b2879b6cf61fb36842bc3feabfb
Yuta Saito <kateinoigakukun@gmail.com>
2020-02-06 16:22:04 +0900

    Initial commit

---
+++ XcodeGenWithPods/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  XcodeGenWithPods
+//
+//  Created by kateinoigakukun on 2020/02/06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
